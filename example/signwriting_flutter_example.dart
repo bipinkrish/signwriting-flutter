9ce62f58385ccf755288d7b9277c68f0a4991f88
Bipin <bipinkrish4@gmail.com>
2024-10-26 19:27:59 +0530

    update example

--- example/signwriting_flutter_example.dart
+++ example/signwriting_flutter_example.dart
@@ -6,6 +6,34 @@ void main() {
   runApp(const MyApp());
 }
 
+class SignWritingWidget extends StatelessWidget {
+  final String fsw;
+  const SignWritingWidget({required this.fsw, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Uint8List>(
+      future: signwritingToImage(
+        fsw,
+        trustBox: false,
+        lineColor: Colors.deepOrange,
+        fillColor: Colors.white,
+      ),
+      builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return Image.memory(snapshot.data!);
+          } else {
+            return const Text('Failed to render SignWriting Image');
+          }
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,25 +46,10 @@ class MyApp extends StatelessWidget {
           title: const Text('SignWriting Image Test'),
         ),
         backgroundColor: Colors.black,
-        body: Center(
-          child: FutureBuilder<Uint8List>(
-            future: signwritingToImage(
-              'AS10011S10019S2e704S2e748M525x535S2e748483x510S10011501x466S20544510x500S10019476x475',
-              trustBox: false,
-              lineColor: Colors.deepOrange,
-              fillColor: Colors.white,
-            ),
-            builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return Image.memory(snapshot.data!);
-                } else {
-                  return const Text('Failed to render SignWriting image');
-                }
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
+        body: const Center(
+          child: SignWritingWidget(
+            fsw:
+                "AS10011S10019S2e704S2e748M525x535S2e748483x510S10011501x466S20544510x500S10019476x475",
           ),
         ),
       ),
